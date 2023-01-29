51509d2c0b9fdf651198605880b7af8c4e953826
KarLOL82 <klinfeldt@gmail.com>
2023-01-29 14:00:09 -0800

    building footerBanner and Layout components

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React from 'react';
+import Head from 'next/head';
+import Navbar from './Navbar';
 
 const Layout = () => {
   return (
-    <div>Layout</div>
+    <div className='layout'>
+      <Head>
+        <title>Clever Store Name</title>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className='main-container'>
+
+      </main>
+        
+    </div>
   )
 }
 
