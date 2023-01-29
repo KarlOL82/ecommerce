e488b771c399bd0032d81c3447b7d3c7d96f273b
KarLOL82 <klinfeldt@gmail.com>
2023-01-28 21:27:30 -0800

    resolving rendering issue of Home

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React from 'react';
 
-import { Product, FooterBanner, Herobanner } from "@/components";
+import { client } from '../lib/client';
+import { Product, FooterBanner, HeroBanner } from '../components';
 
 const Home = () => (
   <div>
-    <Herobanner />
+    <HeroBanner />
 
     <div className="products-heading">
       <h2>Best Selling Products</h2>
@@ -18,5 +19,17 @@ const Home = () => (
     <FooterBanner />
   </div>
 );
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData }
+  }
+}
 
 export default Home;
