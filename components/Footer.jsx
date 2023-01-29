d4f6dc56ab2ad9ebbc9af25cc99a5f25d2e2efeb
KarLOL82 <klinfeldt@gmail.com>
2023-01-29 15:27:17 -0800

    rendering slug.js for product details

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,20 +1,16 @@
-import React from 'react';
-import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai';
+import React from "react";
+import { AiFillInstagram, AiOutlineTwitter } from "react-icons/ai";
 
 const Footer = () => {
   return (
-    <div className='footer-container'>
-      <p>
-        2023 CSN Headphones All Rights Reserved
-      </p>
-      <p className='icons'>
+    <div className="footer-container">
+      <p>2023 CSN Headphones All Rights Reserved</p>
+      <p className="icons">
         <AiFillInstagram />
         <AiOutlineTwitter />
-
       </p>
-
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
