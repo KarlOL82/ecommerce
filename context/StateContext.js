7ce14b9c7d28f67557b995f321296ac6f6becd12
KarLOL82 <klinfeldt@gmail.com>
2023-01-29 19:33:28 -0800

    adding products to cart and checking if item already exists in cart with conditional state

--- context/StateContext.js
+++ context/StateContext.js
@@ -6,10 +6,39 @@ const Context = createContext();
 
 export const StateContext = ({ children }) => {
   const [showCart, setshowCart] = useState(false);
-  const [cartItems, setCartItems] = useState();
-  const [totalPrice, settotalPrice] = useState();
-  const [totalQuantities, settotalQuantities] = useState();
+  const [cartItems, setCartItems] = useState([]);
+  const [totalPrice, setTotalPrice] = useState();
+  const [totalQuantities, setTotalQuantities] = useState();
   const [qty, setQty] = useState(1);
+
+  const onAdd = (product, quantity) => {
+    const checkProductInCart = cartItems.find(
+      (item) => item._id === product._id
+    );
+
+    setTotalPrice(
+      (prevTotalPrice) => prevTotalPrice + product.price * quantity
+    );
+    setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + quantity);
+
+    if (checkProductInCart) {
+      const updatedCartItems = cartItems.map((cartProduct) => {
+        if (cartProduct._id === product._id)
+          return {
+            ...cartProduct,
+            quantity: cartProduct.quantity + quantity,
+          };
+      });
+
+      setCartItems(updatedCartItems);
+      
+    } else {
+      product.quantity = quantity;
+
+      setCartItems([...cartItems, { ...product}]);
+    }
+    toast.success(`${qty} ${product.name} added to your cart.`);
+  };
 
   const incQty = () => {
     setQty((prevQty) => prevQty + 1);
@@ -18,9 +47,9 @@ export const StateContext = ({ children }) => {
     setQty((prevQty) => {
       if (prevQty - 1 < 1) return 1;
 
-      return prevQty -1;
+      return prevQty - 1;
     });
-  }
+  };
 
   return (
     <Context.Provider
@@ -32,6 +61,7 @@ export const StateContext = ({ children }) => {
         qty,
         incQty,
         decQty,
+        onAdd
       }}
     >
       {children}
