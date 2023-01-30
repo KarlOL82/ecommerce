d5289350fcb8c8967ed8d8060160ebf1c15d24a6
KarLOL82 <klinfeldt@gmail.com>
2023-01-29 18:56:48 -0800

    adding incQty and decQty functionality

--- context/StateContext.js
+++ context/StateContext.js
@@ -1,26 +1,40 @@
-import React, { createContext, useContext, useState, useEffect} from 'react';
+import React, { createContext, useContext, useState, useEffect } from "react";
 
-import { toast } from 'react-hot-toast';
+import { toast } from "react-hot-toast";
 
 const Context = createContext();
 
-export const StateContext = ( { children }) => {
-   const [showCart, setshowCart] = useState(false); 
-   const [cartItems, setCartItems] = useState();
-   const [totalPrice, settotalPrice] = useState();
-   const [totalQuantities, settotalQuantities] = useState();
-   const [qty, setQty] = useState(1)
+export const StateContext = ({ children }) => {
+  const [showCart, setshowCart] = useState(false);
+  const [cartItems, setCartItems] = useState();
+  const [totalPrice, settotalPrice] = useState();
+  const [totalQuantities, settotalQuantities] = useState();
+  const [qty, setQty] = useState(1);
 
-   return(
+  const incQty = () => {
+    setQty((prevQty) => prevQty + 1);
+  };
+  const decQty = () => {
+    setQty((prevQty) => {
+      if (prevQty - 1 < 1) return 1;
+
+      return prevQty -1;
+    });
+  }
+
+  return (
     <Context.Provider
-        value={{
-          showCart,
-          cartItems,
-          totalPrice,
-          totalQuantities,
-          qty  
-        }}>
-       {children} 
+      value={{
+        showCart,
+        cartItems,
+        totalPrice,
+        totalQuantities,
+        qty,
+        incQty,
+        decQty,
+      }}
+    >
+      {children}
     </Context.Provider>
-   )
-}
+  );
+};
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -8,6 +8,7 @@ import {
 
 import { client, urlFor } from "../../lib/client";
 import { Product } from "../../components";
+import { useStateContext } from "../../context/StateContext"
 
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
