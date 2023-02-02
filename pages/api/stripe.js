8cbd60eb529dc0d72ca569b7d1df0907fad4908a
KarLOL82 <klinfeldt@gmail.com>
2023-02-01 19:22:43 -0800

    req.body.cartItems returning as undefined

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -1,4 +1,4 @@
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const stripe = require("stripe")(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
@@ -22,12 +22,13 @@ export default async function handler(req, res) {
       };
       // Create Checkout Sessions from body params.
       const session = await stripe.checkout.sessions.create(params);
+
       res.status(200).json(session);
     } catch (err) {
       res.status(err.statusCode || 500).json(err.message);
     }
   } else {
-    res.setHeader("Allow", "POST");
-    res.status(405).end("Method Not Allowed");
+    res.setHeader('Allow', 'POST');
+    res.status(405).end('Method Not Allowed');
   }
 }
