7c9e3031bcfdbedddda5ee79703f0e98d77077e1
KarLOL82 <klinfeldt@gmail.com>
2023-02-02 20:31:35 -0800

    adding buy now feature for expedited checkout

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -10,10 +10,17 @@ import { client, urlFor } from "../../lib/client";
 import { Product } from "../../components";
 import { useStateContext } from "../../context/StateContext"
 
+
+
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
   const [index, setIndex] = useState(0);
-  const { decQty, incQty, qty, onAdd } = useStateContext();
+  const { decQty, incQty, qty, onAdd, setShowCart } = useStateContext();
+  const handleBuyNow = () => {
+    onAdd(product, qty);
+  
+    setShowCart(true);
+  }
   return (
     <div>
       <div className="product-detail-container">
@@ -69,7 +76,7 @@ const ProductDetails = ({ product, products }) => {
             <button type="button" className="add-to-cart" onClick={() => onAdd(product, qty)}>
               Add To Cart
             </button>
-            <button type="button" className="buy-now" onClick="">
+            <button type="button" className="buy-now" onClick={handleBuyNow}>
               Buy Now
             </button>
           </div>
