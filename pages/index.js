14af1115021c658102cc1f4838f6c69e419cd97a
KarLOL82 <klinfeldt@gmail.com>
2023-02-11 14:46:21 -0800

    updating landing page text

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ const Home = ({ products, bannerData }) => (
     <HeroBanner heroBanner={bannerData.length && bannerData[0]}  />
       {/* {console.log(bannerData)} */}
     <div className="products-heading">
-      <h2>Best Selling Products</h2>
+      <h2>Top Selling Products</h2>
       <p>Premium Audio Devices</p>
     </div>
 
